9e2cf7d5315723815d445dcc22434e68676ae621
ilgo <ilgo711@gmail.com>
2010-01-21 20:20:52 +0700

    initial import for a Test code

---
+++ pitaka-test/src/zen/ilgo/pitaka/dict/DictTest.java
@@ -0,0 +1,59 @@
+package zen.ilgo.pitaka.dict;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import zen.ilgo.pitaka.dict.star.Stardict;
+
+public class DictTest {
+
+	static IDict instance;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		File ifo = new File("/home/ilgo/.stardict/dic/Bulkwang.ifo");
+		instance = new Stardict(ifo);
+	}
+
+	@Test
+	public void testGetAuthor() {
+		String author = instance.getAuthor();
+
+		System.out.println(author);
+
+		assertTrue("ilgo Sunim (釋日高)".equals(author));
+	}
+
+	@Test
+	public void testGetEntryCount() {
+		int entryCount = instance.getEntryCount();
+		assertTrue(entryCount == 22646);
+	}
+
+	@Test
+	public void testGetName() {
+		String name = instance.getName();
+
+		System.out.println(name);
+
+		assertTrue("Bulkwang".equals(name));
+	}
+
+	@Test
+	public void testNext() {
+
+		int counter = 0;
+		while (instance.hasNext()) {
+			instance.next();
+			counter++;
+		}
+		instance.dispose();
+		assertTrue(counter == instance.getEntryCount());
+	}
+
+}
